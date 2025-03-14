0e29c2ec8b82fdb264dda0a8373867e0fc325d62
2005101 <aminaelguenuni2005amina@gmail.com>
2025-03-14 16:45:48 -0400

    commit

--- pa04/pa04.java
+++ pa04/pa04.java
@@ -1,24 +1,41 @@
-package pa04;
+
 import java.util.*;
 
 public class pa04 {
     public static void main(String[] args) {
         Scanner keyboard=new Scanner(System.in);
+        System.out.println("Please enter a mathematical expression: ");
         String input=keyboard.nextLine();
 
         Stack<Character> s=new Stack<Character>();
-        //process
-        //read one char at a time from left to right 
+        Stack<Integer> position = new Stack<>();
+
         for (int index = 0; index <input.length(); index++) {
             char c=input.charAt(index);
-            //if C is ''(' or { or []} then push c into s
-            // if c is a closing symbol then pop stack, and check the popped value matches c/
-            
-            
-        } 
+            if (c=='('|| c=='{'|| c=='[') {
+                s.push(c);
+                position.push(index);
+                System.out.println(s);
+            }
+            if (c==')'|| c=='}'|| c==']') {
+                char top = s.pop();
+                position.pop();
+                System.out.println(s); 
 
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    System.out.println("The input expression is not balanced. The first mismatch is found at position " + index + "!");
+                    return;
+                }
 
-        //output 
+            }              
+        }
+        if (s.isEmpty()) {
+            System.out.println("The input expression is balanced!");
+            
+        }
+
     }
     
 }
